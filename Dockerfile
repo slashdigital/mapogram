e9d1e90783b2732842a8985b6594f847da0d55b5
Sean Nuon <sean@slash.co>
2022-04-04 15:02:45 +0700

    update the dockerfile, and fix the build async not finishing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12
+FROM node:17.8-alpine
 
 # Create app directory
 RUN mkdir -p /usr/src/app
@@ -13,7 +13,6 @@ COPY . .
 
 # Building app
 RUN yarn run build
-EXPOSE 3000
 
 # Running the app
-CMD ["yarn", "start"]
+CMD ["yarn", "dev"]
--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
 const nextBuild = require("next/dist/build").default;
 const nextConfig = require("../config/next");
-
-nextBuild(nextConfig.dir, nextConfig.config);
+nextBuild(nextConfig.dir, nextConfig.config, false, true, false).then((e) => {
+  console.log('Build successfully');
+  process.exit(0);
+});

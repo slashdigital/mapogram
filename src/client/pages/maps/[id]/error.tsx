4206032f979d8ddd528df0301aacefe8141d5782
Sean Nuon <sean@slash.co>
2022-05-05 18:27:28 +0700

    fix style error page

--- src/client/pages/maps/[id]/error.tsx
+++ src/client/pages/maps/[id]/error.tsx
@@ -4,6 +4,12 @@ import { withRouter, NextRouter, Router } from 'next/router'
 
 import Layout from "../../../components/Layout";
 import GenerationFailed from "../../../components/maps/GenerationFailed"
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Ribbon from '../../../components/Ribbon';
+
+import Styles from "./status.module.css";
 
 interface WithRouterProps {
   router: NextRouter
@@ -16,7 +22,21 @@ interface MapStatusPageProps extends WithRouterProps {
 const MapGenerationErrorPage = (props: MapStatusPageProps) => {
   return (
     <Layout title="Map Generating Error">
-      <GenerationFailed />
+      
+
+      <Ribbon />
+
+      <Box className={Styles.status_page}>
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sx={{ alignItems: "center", display: "flex" }}>
+              <Container maxWidth="lg">
+                <GenerationFailed />
+              </Container>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
       
     </Layout>
   );

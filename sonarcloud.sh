1d42bb9fdabe53f463512688db5e2570dba3c4f2
Ardian Asah <76830663+ekkeid-service-user@users.noreply.github.com>
2022-08-30 16:41:49 +0700

    SonarCloud pipeline

--- sonarcloud.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sonar-scanner.bat \
-  -D"sonar.organization=slash-foundry" \
-  -D"sonar.projectKey=mapogram-webapp" \
-  -D"sonar.sources=." \
-  -D"sonar.host.url=https://sonarcloud.io"
-  

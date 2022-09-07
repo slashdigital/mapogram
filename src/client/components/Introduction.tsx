b1bbc5d5638895a4a9ac2643e09ad898cfc333a9
SO Vengleab <vengleab@slash.co>
2022-09-07 10:11:47 +0700

    Fix eslint

--- src/client/components/Introduction.tsx
+++ src/client/components/Introduction.tsx
@@ -1,7 +1,9 @@
-export default () => (
+const Introduction = () => (
   <>
-    Mapogram is a platform powered by containerized<b> QGIS server</b>, which supported by any container
-    infrastructure, to provide map generation service for the map they needed to deal with natural
-    disaster like Forest fire, Flood event etc...
+    Mapogram is a platform powered by containerized<b> QGIS server</b>, which supported by any
+    container infrastructure, to provide map generation service for the map they needed to deal with
+    natural disaster like Forest fire, Flood event etc...
   </>
 );
+
+export default Introduction;
